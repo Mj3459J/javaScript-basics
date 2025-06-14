1ed5a4b53775f72366b0e4753d5656c26de67591
Mj3459J <138866558+Mj3459J@users.noreply.github.com>
2025-06-14 14:36:50 +0000

    learned about dataTypes and ecma

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not browser, there different syntax in nodejs
+// Premetive DataTypes:
+let name="Mayur"// string
+let age=21// number
+let isTrue=true// boolean
+let temperature=null// null => standalone value. empty.
+let car;// undefined=> value yet not assigned
+// symbol => unique
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
