31faf297ae7a2e8ca6a2265f1315cfd2e9533473
Mj3459J <138866558+Mj3459J@users.noreply.github.com>
2025-06-14 13:58:52 +0000

    learned about variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accId=242242
+let accEmail="mayursinhjadeja3339@gmail.com"
+var accPass="1234"// never going to use it as it has scope problem .Thats why we use let for variables.
+accCity="Vadodara"
+let accState;
+
+// accId=2 // Not Allowed
+accEmail="mj@gmail.com"
+accPass="111"
+accCity="Gandhinagar"
+
+console.table([accId,accEmail,accPass,accCity,accState])
