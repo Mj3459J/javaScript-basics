06d60dd442488b981c98eaeddedf207f2ce84edd
Mj3459J <138866558+Mj3459J@users.noreply.github.com>
2025-06-15 07:38:19 +0000

    learned conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score="33a"
+
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// Conversion in Number
+// "33"=> 33
+// "33abc"=> NaN
+// true=> 1
+
+let isLoggedIn="MJ";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// Conversion in Boolean
+// 1=>ture
+// ""=> false
+// "MJ"=>true
+//  null=> false
+
+let num=null
+let strNum=String(num)
+// console.log(strNum);
+// console.log(typeof(strNum));
+
+// Conversion in String
+// 33=> "33"
+// true=>"true"
+// null=>"null"
