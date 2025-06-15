1eb10183e0bb1535750dec3b861e1c644e0cae14
Mj3459J <138866558+Mj3459J@users.noreply.github.com>
2025-06-15 09:02:23 +0000

    learned primitive datatypes

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,23 @@
+/* Primitive DataTypes.
+-> 7 Types: 
+1.String
+2.Number
+3.Boolean
+4.null
+5.undefined
+
+6.Symbol
+const id=Symbol('123')
+const id2=Symbol('123')
+console.log(id===id2)// false
+
+7.BigInt
+const bigNumber=3457876666n
+
+*/
+
+/* Reference (Non Premitive) DataTypes.
+1.Arrays
+2.Objects
+3.Functions
+*/
