99bd3aa979ca89d862bf9bc3260c2d5c6c27a63b
Jadeja Mayursinh <138866558+Mj3459J@users.noreply.github.com>
2025-06-19 13:09:07 +0000

    learned about function,IIFE,Arroy fun

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,5 +18,32 @@ function handleObject(obj){
     console.log(`Username is ${obj.username} and price is ${obj.price}`);
     
 }
-handleObject(user);
+// handleObject(user);
 
+
+//****** Arroy Function.
+
+// const chai=()=>{
+//     console.log("Hello");   
+// }
+// chai()
+
+// Implicit return
+
+// const chai=(a,b)=> a+b
+// const chai=(a,b)=> (a+b)
+// const chai = () => ({name:"MJ"}) // if want to return obj then need to use perenthesis.
+// console.log(chai());
+
+
+/********* IIFE (Immediately Invoked Function Expression) **********/
+// use semi-colen on the line before this otherwise error.
+
+// named IIFE
+(function chaii(){
+    console.log(`DB Connected`); 
+})();
+
+((name)=>{
+    console.log(`DB Connected ${name}`); 
+})("MJ");
